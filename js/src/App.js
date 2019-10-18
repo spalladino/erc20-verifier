19fa3bf6023304d8f54c7c573885724d44160510
Santiago Palladino <spalladino@gmail.com>
2019-10-18 16:29:46 -0300

    Add link to forum

--- js/src/App.js
+++ js/src/App.js
@@ -14,6 +14,7 @@ function App() {
       <div className="App-body">
         <Checker />
         <p className="App-notes">The contract source code must be verified on etherscan, and be compiled with Solidity v0.5. This page uses <a href="https://github.com/tinchoabbate/slither-scripts/tree/master/erc20">tinchoabbate's slither-scripts</a> to check whether a contract is a valid ERC20 or not using Slither. The script does not verify that the functions found behave as expected. It just checks for matching signatures, return types, existence of custom modifiers, event emissions, among others. These scripts may have bugs, use them at your own risk.</p>
+        <p className="App-notes">Have any feedback? Share any thoughts in the <a href="https://forum.openzeppelin.com/t/online-erc20-contract-verifier/1575" target="_blank">OpenZeppelin forum.</a></p>
       </div>
     </div>
   );
